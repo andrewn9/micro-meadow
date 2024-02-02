8c6a3d2392d8bd8649523de4ccd91a97acb277e5
Lance Phung <lncphung@gmail.com>
2024-02-01 19:41:49 -0500

    comments GRAh

--- src/user.ts
+++ src/user.ts
@@ -1,8 +1,8 @@
 // user.js
 import { Peer } from 'peerjs';
 
+// Create reactive proxy that updates real user metadata and saves it to session storage
 export let metadata = new Proxy<{ [index: string]: unknown }>({}, {
-
     set(target, name, value, receiver) {
         if (typeof(name) === "string") {
             console.log(`Set ${name} to ${value}`);
@@ -20,6 +20,10 @@ export let metadata = new Proxy<{ [index: string]: unknown }>({}, {
 
 let user: Peer;
 
+/**
+ * Gets PeerJS user and UUID.
+ * @returns PeerJS Peer
+ */
 export async function initUser() {
     const raw = sessionStorage.getItem('user');
 
@@ -27,6 +31,7 @@ export async function initUser() {
         const savedUser = JSON.parse(raw);
         user = new Peer(savedUser.id);
 
+        // inefficient - change later
         for (const name in savedUser.metadata) {
             metadata[name] = savedUser[name];
         }
@@ -48,6 +53,9 @@ export async function initUser() {
     });
 }
 
+/**
+ * Saves metadata and UUID to session storage.
+ */
 function saveUserData() {
     const userData = {
         id: user.id,
